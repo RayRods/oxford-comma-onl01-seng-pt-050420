820f73602142690ae76341b6f4fca3ec3eed94c1
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-16 00:36:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join("")
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3
+    array_last = []
+    array_last = array.pop
+    array_last.unshift("and ")
+    array.join(", ")
+    array << array_last
+  end 
 end
